bcd7b0c9fcb55cb4dd457fc052ae75195422bae7
varun thaker <vthakar3@gmail.com>
2023-08-17 11:18:32 +0200

    Done challenge JS-num-var-

---
+++ js-var-num/js-variables-and-numbers_social-media-post/js/cal.js
@@ -0,0 +1,10 @@
+let mealCost = 160;
+let tipPercent = 10;
+
+let tipAmount = mealCost * (tipPercent / 100);
+
+let totalMealCost = mealCost + tipAmount;
+
+console.log("The meal Cost is " + mealCost + "€");
+console.log("Youre tip Amount is " + tipAmount + "€");
+console.log("The total cost of youre meal is " + totalMealCost + "€");
