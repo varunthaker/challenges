d389061775e5c7b938c54148ec387b7d6a8fb17e
varun thaker <vthakar3@gmail.com>
2023-08-23 11:09:44 +0200

    Done form-submit

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //get the Form Data in nested Array Form
+  const formData = new FormData(event.target);
+
+  console.log(formData);
+
+  //converting nested Array into Array with object
+  const data = Object.fromEntries(formData);
+
+  console.log(`Your data is ${JSON.stringify(data)}`);
+});
