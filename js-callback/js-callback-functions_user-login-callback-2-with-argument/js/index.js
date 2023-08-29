d82041393110350f3d9d150f02c64a0ed9cef4d6
varun thaker <vthakar3@gmail.com>
2023-08-29 11:32:40 +0200

    done challenge 2

--- js-callback/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js-callback/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,17 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+//1. Defining callback externally
+
+const showWelcomeMessage = (userName) => {
+  console.log(`Welcome ${userName} You are logged in now.`);
+};
+
+handleUserLogin(showWelcomeMessage);
+
+//2. defining callback within
+
+handleUserLogin((userName) => {
+  console.log(`Again ${userName}! You are welcome.`);
+});
