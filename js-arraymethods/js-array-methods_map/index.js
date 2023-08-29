bedd73f397fc3f1891e94105c1722022e97a81a4
varun thaker <vthakar3@gmail.com>
2023-08-29 15:48:06 +0200

    done challenge map

--- js-arraymethods/js-array-methods_map/index.js
+++ js-arraymethods/js-array-methods_map/index.js
@@ -24,10 +24,20 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+}); // ['as often as you like.', ...]
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return `${card.question} - ${card.answer}`;
+}); // ["How often can I use <header>? - As often as you like.", ...]
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  const questionAndAnswer = {
+    question: card.question,
+    answer: card.answer,
+  };
+  return questionAndAnswer;
+});
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
