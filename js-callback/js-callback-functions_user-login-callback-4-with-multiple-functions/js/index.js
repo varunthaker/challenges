e963cf47e57e91f0eef7b6c7ddd927086957edf1
varun thaker <vthakar3@gmail.com>
2023-08-29 11:53:41 +0200

    done challenge 4

--- js-callback/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js-callback/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,12 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+
+const showWelcomeMessage = (userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+};
+const showErrorMessage = (errorMessage) => {
+  console.log(`Login error: ${errorMessage}`);
+};
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
