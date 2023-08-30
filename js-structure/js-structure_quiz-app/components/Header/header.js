25285476d45741f389b1eaeb73e812d62142322f
varun thaker <vthakar3@gmail.com>
2023-08-30 17:47:54 +0200

    Quiz App done

---
+++ js-structure/js-structure_quiz-app/components/Header/header.js
@@ -0,0 +1,19 @@
+export function Header() {
+  const header = document.createElement("header");
+  header.classList.add("header");
+  header.innerHTML = /* html */ `
+        <h1 class="header__title">Quiz-App</h1>
+      `;
+
+  return header;
+}
+
+// function Header() {
+//   const header = document.createElement("header");
+//   header.classList.add("header");
+//   header.innerHTML = /* html */ `
+// 	  <h1 class="header__title">Quiz-App</h1>
+// 	`;
+
+//   return header;
+// }
