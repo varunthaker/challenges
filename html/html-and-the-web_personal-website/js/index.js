ff621fd26321cabab00c407421b6a7a268f30040
varun thaker <vthakar3@gmail.com>
2023-09-02 17:43:58 +0200

    JS almost done

--- html/html-and-the-web_personal-website/js/index.js
+++ html/html-and-the-web_personal-website/js/index.js
@@ -1,17 +1,47 @@
 console.clear();
 
-const toggleButton = document.querySelector('[data-js="toggle-switch"]');
-const mainContainer = document.querySelector('[data-js="main-container"]');
-const themeContent = document.querySelector("#theme");
+const currentYearElement = document.querySelector('[data-js="year"]');
+const seeMoreButton = document.querySelector('[data-js="see-more"]');
+const hiddenProjectCards = document.querySelectorAll(
+  '[data-js="hidden-element"]'
+);
 
-console.log(themeContent);
+const bodyElement = document.querySelector("body");
+
+//Get Present Year for Footer
+const currentYear = new Date().getFullYear();
+currentYearElement.innerText = currentYear;
+
+//Show all projects
+seeMoreButton.addEventListener("click", () => {
+  if (seeMoreButton.innerText === "Show More") {
+    Array.from(hiddenProjectCards).map((projectCard) =>
+      projectCard.classList.remove("hidden")
+    );
+    seeMoreButton.innerText = "Show Less";
+  } else if (seeMoreButton.innerText === "Show Less") {
+    Array.from(hiddenProjectCards).map((projectCard) =>
+      projectCard.classList.add("hidden")
+    );
+    seeMoreButton.innerText = "Show More";
+  }
+});
+
+const toggleButton = document.querySelector('[data-js="toggle-switch"]');
+const toggleIcon = document.getElementById("theme");
+
 toggleButton.addEventListener("click", () => {
-  console.log(themeContent.classList[2]);
-  if (themeContent.classList === "fa-moon") {
-    console.log("dark mode is active");
-  } else {
-    themeContent.classList.remove("fa-moon");
-    themeContent.classList.add("fa-sun");
-    console.log("lightmode is active");
+  console.log("clicked");
+
+  console.log(toggleIcon.classList[0]);
+
+  if (toggleIcon.classList[2] === "fa-moon") {
+    toggleIcon.classList.remove("fa-moon");
+    toggleIcon.classList.add("fa-sun");
+    bodyElement.classList.add("light-mode");
+  } else if (toggleIcon.classList[2] === "fa-sun") {
+    toggleIcon.classList.remove("fa-sun");
+    toggleIcon.classList.add("fa-moon");
+    bodyElement.classList.remove("light-mode");
   }
 });
