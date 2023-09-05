38179b74d456b2356aa41f69cce9935cd5496392
varun thaker <vthakar3@gmail.com>
2023-09-05 10:54:59 +0200

    done test1

--- unit-test/js-unit-testing_get-animal/index.js
+++ unit-test/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  } else if (animal?.charAt(animal.length - 1) === "s") {
+    return `I like ${animal}!`;
+  } else {
+    return "I do not like animals at all!";
+  }
+}
