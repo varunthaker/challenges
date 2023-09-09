dda1cd521ee05d0af1c0c0f4f2ff14009f497760
varun thaker <vthakar3@gmail.com>
2023-09-09 12:27:56 +0200

    done initial version

--- html/html-and-the-web_personal-website/js/index.js
+++ html/html-and-the-web_personal-website/js/index.js
@@ -49,15 +49,18 @@ const scriptURL =
   "https://script.google.com/macros/s/AKfycbzn418HyXmqNNmO4kLkPp7kVxzWyRoca-OtXxeMrvPbomzRtgc75ANLEPYAS2ZwVBuU/exec";
 const form = document.querySelector("form");
 const nameElement = document.querySelector("#name");
+const submitButton = document.querySelector('[data-js="submit-form"]');
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      alert("Message sent Successfully");
+  submitButton.classList.add("button--loading", "button__text");
+  try {
+    fetch(scriptURL, { method: "POST", body: new FormData(form) }).then(() => {
+      submitButton.classList.remove("button--loading", "button__text");
       form.reset();
       nameElement.focus();
-    })
-    .catch((error) => console.error("Error!", error.message));
+    });
+  } catch (error) {
+    console.error("Error!", error.message);
+  }
 });
