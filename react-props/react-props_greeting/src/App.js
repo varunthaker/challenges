248ec7a4199bea7fbc87feaceb1f0980bfb11f05
varun thaker <vthakar3@gmail.com>
2023-09-11 15:57:27 +0200

    initial files

---
+++ react-props/react-props_greeting/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Greetings from "./Greetings";
+
+export default function App() {
+  return (
+    <>
+      <Greetings name="Varun" />
+      <Greetings name="Vijay" />
+    </>
+  );
+}
---
+++ react-props/react-props_greeting/src/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Greetings = ({ name }) => {
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+    </div>
+  );
+};
+
+export default Greetings;
