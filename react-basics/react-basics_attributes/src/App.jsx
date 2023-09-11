b412e1b767030dad0c0e2cd9949fe022a1613400
varun thaker <vthakar3@gmail.com>
2023-09-11 12:13:14 +0200

    done challenege 3

---
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Article from "./Article";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Article />
+    </div>
+  );
+}
---
+++ react-basics/react-basics_attributes/src/Article.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <>
+      <article className="article">
+        <h2 className="article__title">Article Title:</h2>
+        <label htmlFor="input"></label>
+        <input type="text" id="input" name="input" />
+        <a className="article__link" href="#">
+          Link
+        </a>
+      </article>
+    </>
+  );
+};
+
+export default Article;
