4bf6477368eb5bc78bc7e2b8ee0842d88f2f070f
varunthaker <vthakar3@gmail.com>
2023-09-12 09:17:54 +0200

    Update react-basics/react-basics_attributes/src/Article.jsx
    Co-authored-by: reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>

--- react-basics/react-basics_attributes/src/Article.jsx
+++ react-basics/react-basics_attributes/src/Article.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Article = () => {
   return (
-    <>
       <article className="article">
         <h2 className="article__title">Article Title:</h2>
-        <label htmlFor="input"></label>
+        <label htmlFor="input">Some label</label>
         <input type="text" id="input" name="input" />
         <a className="article__link" href="#">
           Link
         </a>
       </article>
-    </>
   );
 };
 
