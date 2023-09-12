52f25c260d490365bada031005b2e488183e038f
varunthaker <vthakar3@gmail.com>
2023-09-12 09:16:59 +0200

    Update App.js

--- react-basics/react-basics_hello-world-article/src/App.js
+++ react-basics/react-basics_hello-world-article/src/App.js
@@ -3,20 +3,20 @@ import "./styles.css";
 
 const HelloWorldArticle = () => {
   return (
-    <>
+    
       <article>
         {" "}
         <h1>Hello World</h1>
         <p>This is the hello World Article</p>
       </article>
-    </>
+    
   );
 };
 
 export default function App() {
   return (
-    <div>
+    
       <HelloWorldArticle />
-    </div>
+    
   );
 }
