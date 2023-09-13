56793c07a247aded2b33a14a2ec0113d00d1ce56
varun thaker <vthakar3@gmail.com>
2023-09-13 10:56:46 +0200

    Challenge: Sum with props

--- react-prop/react-props_sum/src/App.js
+++ react-prop/react-props_sum/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum num1={2} num2={5} />;
+}
+
+function Sum({ num1, num2 }) {
+  return <h1>The Sum is {num1 + num2}</h1>;
 }
