760ebb9b09b4fe4199a78d0764bcd0bebf7d64db
varun thaker <vthakar3@gmail.com>
2023-09-13 10:41:47 +0200

    done challenge greeting

--- react-prop/react-props_greeting/src/App.js
+++ react-prop/react-props_greeting/src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Jan" />;
 }
+
+const Greeting = ({ name }) => {
+  return name === "Jan" ? <h1>Hello Coach!</h1> : <h1>Hello {name}!</h1>;
+};
