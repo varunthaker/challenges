47fa2ae5ab33c6739d918a6193df00c8ed2e1b5d
varun thaker <vthakar3@gmail.com>
2023-09-14 11:31:15 +0200

    Table Reservation Challenege

--- react-state2/react-state-2_table-reservation/src/App.js
+++ react-state2/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Counter from "./components/Counter";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter peopleNumber={people} onPeopleChange={setPeople} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
